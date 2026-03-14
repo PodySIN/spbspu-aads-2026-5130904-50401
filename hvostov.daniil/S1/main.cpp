ef1a954687fffe210661f3dfab275c3147f6113f
PodySIN <daniil2007xboctob@gmail.com>
2026-03-15 00:31:02 +0300

    Fixing overflow...

--- hvostov.daniil/S1/main.cpp
+++ hvostov.daniil/S1/main.cpp
@@ -8,6 +8,12 @@ int main()
   try {
     list = hvostov::getData(std::cin);
   } catch (const std::overflow_error& e) {
+    try {
+      hvostov::printResult(std::cout, list);
+    } catch (const std::overflow_error& e) {
+      std::cerr << e.what() << "\n";
+      return 1;
+    }
     std::cerr << e.what() << "\n";
     return 1;
   }
