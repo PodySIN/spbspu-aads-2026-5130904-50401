bb7ed4fe2cb66fd3c85f1b791fc4d25b209161c2
PodySIN <daniil2007xboctob@gmail.com>
2026-05-16 23:57:09 +0300

    Add commands

---
+++ hvostov.daniil/S3/commands.hpp
@@ -0,0 +1,27 @@
+#ifndef COMMANDS_HPP
+#define COMMANDS_HPP
+
+#include "graph.hpp"
+#include "hash_table.hpp"
+#include "sip_hash.hpp"
+#include <istream>
+#include <ostream>
+#include <string>
+
+namespace hvostov {
+
+  using GraphTable = HashTable< std::string, Graph, SipHash< std::string >, std::equal_to< std::string > >;
+
+  void graphs(std::istream& in, std::ostream& out, GraphTable& graphs);
+  void vertexes(std::istream& in, std::ostream& out, GraphTable& graphs);
+  void outbound(std::istream& in, std::ostream& out, GraphTable& graphs);
+  void inbound(std::istream& in, std::ostream& out, GraphTable& graphs);
+  void bind(std::istream& in, std::ostream& out, GraphTable& graphs);
+  void cut(std::istream& in, std::ostream& out, GraphTable& graphs);
+  void create(std::istream& in, std::ostream& out, GraphTable& graphs);
+  void merge(std::istream& in, std::ostream& out, GraphTable& graphs);
+  void extract(std::istream& in, std::ostream& out, GraphTable& graphs);
+
+}
+
+#endif
