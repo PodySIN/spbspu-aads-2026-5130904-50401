b16003031ffec0e6a41eb24ffa6c355fb905858e
PodySIN <daniil2007xboctob@gmail.com>
2026-05-16 23:54:20 +0300

    Add constructors and opeartor=

--- hvostov.daniil/S3/hash_table.hpp
+++ hvostov.daniil/S3/hash_table.hpp
@@ -76,4 +76,164 @@ namespace hvostov {
   };
 
 }
+
+template < class Key, class Value, class Hash, class Equal >
+size_t hvostov::HashTable< Key, Value, Hash, Equal >::overflowStart() const noexcept
+{
+  return bucket_count_ * bucket_size_;
+}
+
+template < class Key, class Value, class Hash, class Equal >
+size_t hvostov::HashTable< Key, Value, Hash, Equal >::totalCapacity() const noexcept
+{
+  return overflowStart() + overflow_cap_;
+}
+
+template < class Key, class Value, class Hash, class Equal >
+void hvostov::HashTable< Key, Value, Hash, Equal >::allocate(size_t num_buckets)
+{
+  size_t new_bucket_count = num_buckets;
+  size_t new_overflow_cap = std::max(size_t(4), num_buckets / 2);
+  size_t new_total_size = new_bucket_count * bucket_size_ + new_overflow_cap;
+
+  std::pair< Key, Value >* new_data = new std::pair< Key, Value >[new_total_size];
+  try {
+    size_t* new_bucket_sizes = new size_t[new_bucket_count]();
+
+    delete[] data_;
+    delete[] bucket_sizes_;
+
+    data_ = new_data;
+    bucket_sizes_ = new_bucket_sizes;
+    bucket_count_ = new_bucket_count;
+    overflow_cap_ = new_overflow_cap;
+    size_ = 0;
+    overflow_size_ = 0;
+
+  } catch (...) {
+    delete[] new_data;
+    throw;
+  }
+}
+
+template < class Key, class Value, class Hash, class Equal >
+hvostov::HashTable< Key, Value, Hash, Equal >::HashTable(size_t expected_elements) :
+  bucket_size_(4),
+  hasher_(),
+  equal_()
+{
+  if (expected_elements == 0) {
+    return;
+  }
+  constexpr double default_load_factor = 0.7;
+  size_t needed_capacity = static_cast< size_t >(std::ceil(expected_elements / default_load_factor));
+  if (needed_capacity <= bucket_size_) {
+    bucket_count_ = 1;
+  } else {
+    bucket_count_ = needed_capacity / bucket_size_;
+  }
+  allocate(bucket_count_);
+}
+
+template < class Key, class Value, class Hash, class Equal >
+hvostov::HashTable< Key, Value, Hash, Equal >::HashTable(const HashTable& other) :
+  bucket_size_(other.bucket_size_),
+  bucket_count_(other.bucket_count_),
+  overflow_cap_(other.overflow_cap_),
+  overflow_size_(other.overflow_size_),
+  size_(other.size_),
+  hasher_(other.hasher_),
+  equal_(other.equal_)
+{
+  if (other.bucket_count_ == 0) {
+    data_ = nullptr;
+    bucket_sizes_ = nullptr;
+    return;
+  }
+  size_t total_size = overflowStart() + overflow_cap_;
+  std::pair< Key, Value >* new_data = new std::pair< Key, Value >[total_size];
+  try {
+    size_t* new_bucket_sizes = new size_t[bucket_count_];
+    for (size_t i = 0; i < total_size; ++i) {
+      new_data[i] = other.data_[i];
+    }
+    for (size_t i = 0; i < bucket_count_; ++i) {
+      new_bucket_sizes[i] = other.bucket_sizes_[i];
+    }
+    data_ = new_data;
+    bucket_sizes_ = new_bucket_sizes;
+  } catch (...) {
+    delete[] new_data;
+    throw;
+  }
+}
+
+template < class Key, class Value, class Hash, class Equal >
+hvostov::HashTable< Key, Value, Hash, Equal >::HashTable(HashTable&& other) noexcept :
+  bucket_size_(other.bucket_size_),
+  data_(other.data_),
+  bucket_sizes_(other.bucket_sizes_),
+  bucket_count_(other.bucket_count_),
+  overflow_cap_(other.overflow_cap_),
+  overflow_size_(other.overflow_size_),
+  size_(other.size_),
+  hasher_(std::move(other.hasher_)),
+  equal_(std::move(other.equal_))
+{
+  other.data_ = nullptr;
+  other.bucket_sizes_ = nullptr;
+  other.bucket_count_ = 0;
+  other.overflow_cap_ = 0;
+  other.overflow_size_ = 0;
+  other.size_ = 0;
+}
+
+template < class Key, class Value, class Hash, class Equal >
+hvostov::HashTable< Key, Value, Hash, Equal >::~HashTable()
+{
+  delete[] data_;
+  delete[] bucket_sizes_;
+}
+
+template < class Key, class Value, class Hash, class Equal >
+hvostov::HashTable< Key, Value, Hash, Equal >&
+hvostov::HashTable< Key, Value, Hash, Equal >::operator=(const HashTable& other)
+{
+  if (this == std::addressof(other)) {
+    return *this;
+  }
+  HashTable tmp(other);
+  swap(tmp);
+  return *this;
+}
+
+template < class Key, class Value, class Hash, class Equal >
+hvostov::HashTable< Key, Value, Hash, Equal >&
+hvostov::HashTable< Key, Value, Hash, Equal >::operator=(HashTable&& other) noexcept
+{
+  if (this == std::addressof(other)) {
+    return *this;
+  }
+  delete[] data_;
+  delete[] bucket_sizes_;
+
+  bucket_size_ = other.bucket_size_;
+  data_ = other.data_;
+  bucket_sizes_ = other.bucket_sizes_;
+  bucket_count_ = other.bucket_count_;
+  overflow_cap_ = other.overflow_cap_;
+  overflow_size_ = other.overflow_size_;
+  size_ = other.size_;
+  hasher_ = std::move(other.hasher_);
+  equal_ = std::move(other.equal_);
+
+  other.data_ = nullptr;
+  other.bucket_sizes_ = nullptr;
+  other.bucket_count_ = 0;
+  other.overflow_cap_ = 0;
+  other.overflow_size_ = 0;
+  other.size_ = 0;
+  return *this;
+}
+
 #endif
