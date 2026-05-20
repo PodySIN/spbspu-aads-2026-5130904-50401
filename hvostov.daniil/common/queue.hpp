acf3109a87292f5ee86ab909426665d2b7090763
PodySIN <daniil2007xboctob@gmail.com>
2026-05-20 23:11:48 +0300

    Do small refactor for S2

--- hvostov.daniil/common/queue.hpp
+++ hvostov.daniil/common/queue.hpp
@@ -1,78 +1,45 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
-#include <cstddef>
 #include "list.hpp"
-#include <utility>
+#include <cstddef>
 
 namespace hvostov {
-  template< class T >
+  template < class T >
   class Queue {
-    public:
-      Queue();
-      Queue(const Queue< T >& queue);
-      Queue(Queue< T >&& queue) noexcept;
-      ~Queue() = default;
-      Queue< T >& operator=(const Queue< T >& queue);
-      Queue< T >& operator=(Queue< T >&& queue) noexcept;
+  public:
+    Queue();
+    Queue(const Queue< T >& queue) = default;
+    Queue(Queue< T >&& queue) noexcept = default;
+    ~Queue() = default;
+    Queue< T >& operator=(const Queue< T >& queue) = default;
+    Queue< T >& operator=(Queue< T >&& queue) noexcept = default;
 
-      T drop();
-      void push(T rhs);
-      void clear() noexcept;
-      bool empty() const noexcept;
-      T& front();
-      const T& front() const;
-      T& back();
-      const T& back() const;
-      size_t getSize() const noexcept;
-    private:
-      List< T > list_;
-      Liter< T > tail_;
+    T drop();
+    void push(const T& rhs);
+    void push(T&& rhs);
+    void clear() noexcept;
+    bool empty() const noexcept;
+    T& front();
+    const T& front() const;
+    T& back();
+    const T& back() const;
+    size_t size() const noexcept;
+
+  private:
+    List< T > list_;
+    Liter< T > tail_;
   };
 }
 
-template< class T >
-hvostov::Queue< T >::Queue():
+template < class T >
+hvostov::Queue< T >::Queue() :
   list_(),
   tail_()
 {
   tail_ = list_.begin();
 }
 
-template< class T >
-hvostov::Queue< T >::Queue(const Queue< T >& queue):
-  list_(queue.list_),
-  tail_(queue.tail_)
-{}
-
-template< class T >
-hvostov::Queue< T >::Queue(Queue< T >&& queue) noexcept:
-  list_(std::move(queue.list_)),
-  tail_(std::move(queue.tail_))
-{}
-
-template< class T >
-hvostov::Queue< T >& hvostov::Queue< T >::operator=(const Queue< T >& queue)
-{
-  if (this == &queue) {
-    return *this;
-  }
-  list_ = queue.list_;
-  tail_ = queue.tail_;
-  return *this;
-}
-
-template< class T >
-hvostov::Queue< T >& hvostov::Queue< T >::operator=(Queue< T >&& queue) noexcept
-{
-  if (this == &queue) {
-    return *this;
-  }
-  list_ = std::move(queue.list_);
-  tail_ = std::move(queue.tail_);
-  return *this;
-}
-
-template< class T >
+template < class T >
 T hvostov::Queue< T >::drop()
 {
   T res = front();
@@ -83,53 +50,59 @@ T hvostov::Queue< T >::drop()
   return res;
 }
 
-template< class T >
-void hvostov::Queue< T >::push(T rhs)
+template < class T >
+void hvostov::Queue< T >::push(const T& rhs)
 {
   tail_ = list_.insertAfter(tail_, rhs);
 }
 
-template< class T >
+template < class T >
+void hvostov::Queue< T >::push(T&& rhs)
+{
+  tail_ = list_.insertAfter(tail_, rhs);
+}
+
+template < class T >
 void hvostov::Queue< T >::clear() noexcept
 {
   list_.clear();
   tail_ = list_.begin();
 }
 
-template< class T >
+template < class T >
 bool hvostov::Queue< T >::empty() const noexcept
 {
   return list_.empty();
 }
 
-template< class T >
+template < class T >
 T& hvostov::Queue< T >::front()
 {
   return *(list_.begin());
 }
 
-template< class T >
+template < class T >
 T& hvostov::Queue< T >::back()
 {
   return *tail_;
 }
 
-template< class T >
+template < class T >
 const T& hvostov::Queue< T >::front() const
 {
   return *(list_.begin());
 }
 
-template< class T >
+template < class T >
 const T& hvostov::Queue< T >::back() const
 {
   return *tail_;
 }
 
-template< class T >
-size_t hvostov::Queue< T >::getSize() const noexcept
+template < class T >
+size_t hvostov::Queue< T >::size() const noexcept
 {
-  return list_.getSize();
+  return list_.size();
 }
 
 #endif
--- hvostov.daniil/common/stack.hpp
+++ hvostov.daniil/common/stack.hpp
@@ -1,68 +1,34 @@
 #ifndef stack_HPP
 #define stack_HPP
-#include <cstddef>
 #include "list.hpp"
-#include <utility>
+#include <cstddef>
 
 namespace hvostov {
-  template< class T >
+  template < class T >
   class Stack {
-    public:
-      Stack();
-      Stack(const Stack< T >& stack);
-      Stack(Stack< T >&& stack) noexcept;
-      ~Stack() = default;
-      Stack< T >& operator=(const Stack< T >& stack);
-      Stack< T >& operator=(Stack< T >&& stack) noexcept;
+  public:
+    Stack() = default;
+    Stack(const Stack< T >& stack) = default;
+    Stack(Stack< T >&& stack) noexcept = default;
+    ~Stack() = default;
+    Stack< T >& operator=(const Stack< T >& stack) = default;
+    Stack< T >& operator=(Stack< T >&& stack) noexcept = default;
 
-      T drop();
-      void push(const T& rhs);
-      void clear() noexcept;
-      bool empty() const noexcept;
-      T& top();
-      const T& top() const;
-      size_t getSize() const noexcept;
-    private:
-      List< T > list_;
+    T drop();
+    void push(const T& rhs);
+    void push(T&& rhs);
+    void clear() noexcept;
+    bool empty() const noexcept;
+    T& top();
+    const T& top() const;
+    size_t size() const noexcept;
+
+  private:
+    List< T > list_;
   };
 }
 
-template< class T >
-hvostov::Stack< T >::Stack():
-  list_()
-{}
-
-template< class T >
-hvostov::Stack< T >::Stack(const Stack< T >& stack):
-  list_(stack.list_)
-{}
-
-template< class T >
-hvostov::Stack< T >::Stack(Stack< T >&& stack) noexcept:
-  list_(std::move(stack.list_))
-{}
-
-template< class T >
-hvostov::Stack< T >& hvostov::Stack< T >::operator=(const Stack< T >& stack)
-{
-  if (this == &stack) {
-    return *this;
-  }
-  list_ = stack.list_;
-  return *this;
-}
-
-template< class T >
-hvostov::Stack< T >& hvostov::Stack< T >::operator=(Stack< T >&& stack) noexcept
-{
-  if (this == &stack) {
-    return *this;
-  }
-  list_ = std::move(stack.list_);
-  return *this;
-}
-
-template< class T >
+template < class T >
 T hvostov::Stack< T >::drop()
 {
   T t = top();
@@ -70,40 +36,46 @@ T hvostov::Stack< T >::drop()
   return t;
 }
 
-template< class T >
+template < class T >
 void hvostov::Stack< T >::push(const T& rhs)
 {
   list_.insertAfter(list_.end(), rhs);
 }
 
-template< class T >
+template < class T >
+void hvostov::Stack< T >::push(T&& rhs)
+{
+  list_.insertAfter(list_.end(), rhs);
+}
+
+template < class T >
 void hvostov::Stack< T >::clear() noexcept
 {
   list_.clear();
 }
 
-template< class T >
+template < class T >
 bool hvostov::Stack< T >::empty() const noexcept
 {
   return list_.empty();
 }
 
-template< class T >
+template < class T >
 T& hvostov::Stack< T >::top()
 {
   return *(list_.begin());
 }
 
-template< class T >
+template < class T >
 const T& hvostov::Stack< T >::top() const
 {
   return *(list_.begin());
 }
 
-template< class T >
-size_t hvostov::Stack< T >::getSize() const noexcept
+template < class T >
+size_t hvostov::Stack< T >::size() const noexcept
 {
-  return list_.getSize();
+  return list_.size();
 }
 
 #endif
