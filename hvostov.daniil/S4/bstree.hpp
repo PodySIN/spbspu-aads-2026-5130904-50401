76d198e64acd6e8b7d81a95ab4b8434e7f5c0265
PodySIN <daniil2007xboctob@gmail.com>
2026-05-23 23:52:04 +0300

    Add node implementation

--- hvostov.daniil/S4/bstree.hpp
+++ hvostov.daniil/S4/bstree.hpp
@@ -1,15 +1,23 @@
 #include <cstddef>
+#include <memory>
 #include <utility>
+#include <stdexcept>
+#include <functional>
 
 namespace hvostov {
 
   namespace detail {
     template < class Key, class Value >
     struct Node {
-      std::pair< const Key, Value > node;
-      Node< Key, Value >*parent, *left, *right;
+      std::pair< const Key, Value > data;
+      Node< Key, Value >* parent;
+      Node< Key, Value >* left;
+      Node< Key, Value >* right;
+      size_t height = 0;
+
       Node() = default;
       Node(const Key& k, const Value& v, Node* p);
+      Node(const Key& k, Value&& v, Node* p);
       ~Node() = default;
     };
   }
@@ -20,9 +28,13 @@ namespace hvostov {
   template < class Key, class Value >
   class BSTConstIterator;
 
-  template < class Key, class Value, class Compare >
+  template < class Key, class Value, class Compare = std::less< Key > >
   class BSTree {
   public:
+    using node_t = detail::Node< Key, Value >;
+    using iterator = BSTIterator< Key, Value >;
+    using const_iterator = BSTConstIterator< Key, Value >;
+
     BSTree();
     BSTree(const BSTree& other);
     BSTree(BSTree&& other) noexcept;
@@ -32,23 +44,24 @@ namespace hvostov {
 
     void push(const Key& k, const Value& v);
     void push(const Key& k, Value&& v);
-    Value get(const Key& k) const;
+    Value& get(const Key& k);
+    const Value& get(const Key& k) const;
     Value drop(const Key& k);
-    using const_iterator = BSTConstIterator< Key, Value >;
+
     const_iterator rotateLeft(const_iterator it);
     const_iterator rotateRight(const_iterator it);
-
     const_iterator rotateLargeRight(const_iterator it);
     const_iterator rotateLargeLeft(const_iterator it);
+
     size_t height(const_iterator it) const;
     size_t height() const;
+
     void swap(BSTree& other) noexcept;
     void clear() noexcept;
 
     bool empty() const noexcept;
     size_t size() const noexcept;
 
-    using iterator = BSTIterator< Key, Value >;
     iterator begin();
     iterator end();
     const_iterator begin() const;
@@ -57,15 +70,23 @@ namespace hvostov {
     const_iterator cend() const noexcept;
 
   private:
-    detail::Node< Key, Value >* root_;
+    node_t* root_;
     size_t size_;
     Compare comp_;
+
+    node_t* copy(node_t* other, node_t* parent);
+    void clearSubtree(node_t* node) noexcept;
+    size_t calcHeight(node_t* node) const;
+    void updateHeightUpwards(node_t* node);
+    node_t* findNode(const Key& k) const;
+    node_t* minimum(node_t* node) const;
+    node_t* maximum(node_t* node) const;
   };
 
   template < class Key, class Value >
   class BSTIterator {
   public:
-    BSTIterator();
+    BSTIterator() = default;
     BSTIterator(const BSTIterator& other) = default;
     BSTIterator(BSTIterator&& other) noexcept = default;
     BSTIterator& operator=(const BSTIterator& other) = default;
@@ -93,7 +114,7 @@ namespace hvostov {
   template < class Key, class Value >
   class BSTConstIterator {
   public:
-    BSTConstIterator();
+    BSTConstIterator() = default;
     BSTConstIterator(const BSTConstIterator& other) = default;
     BSTConstIterator(BSTConstIterator&& other) noexcept = default;
     BSTConstIterator& operator=(const BSTConstIterator& other) = default;
@@ -118,4 +139,24 @@ namespace hvostov {
     BSTConstIterator(detail::Node< Key, Value >* node);
   };
 
+}
+
+template < class Key, class Value >
+hvostov::detail::Node< Key, Value >::Node(const Key& k, const Value& v, Node* p) :
+  data(k, v),
+  parent(p),
+  left(nullptr),
+  right(nullptr),
+  height(0)
+{
+}
+
+template < class Key, class Value >
+hvostov::detail::Node< Key, Value >::Node(const Key& k, Value&& v, Node* p) :
+  data(k, std::move(v)),
+  parent(p),
+  left(nullptr),
+  right(nullptr),
+  height(0)
+{
 }
